89b576db2e01c542fc211d2b0cabe8948fef14cd
Iuliia-Sergeeva <144040954+Iuliia-Sergeeva@users.noreply.github.com>
2023-09-08 19:56:43 +0300

    Update queries.sql
    Первый отчет о десятке лучших продавцов. Таблица состоит из трех колонок - данных о продавце, суммарной выручке с проданных товаров и количестве проведенных сделок, и отсортирована по убыванию выручки

--- queries.sql
+++ queries.sql
@@ -1,2 +1,18 @@
 select count(customer_id) as customers_count
 from customers;
+
+with tab as (
+select 
+concat(e.first_name,' ',e.last_name) as name,
+count (s.quantity) as operations,
+FLOOR (sum (s.quantity * p.price)) as income
+from employees as e
+left join sales as s on s.sales_person_id = e.employee_id
+left join products as p on p.product_id  = s.product_id
+group by e.first_name,e.last_name
+)
+select *
+from tab
+order by income desc nulls last
+limit 10;
+
