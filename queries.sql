ebd167ddbf239103d143b99a19f6c8ecea5851bb
Iuliia-Sergeeva <144040954+Iuliia-Sergeeva@users.noreply.github.com>
2023-09-11 19:03:40 +0300

    Update queries.sql
    Третий отчет содержит информацию о выручке по дням недели. Каждая запись содержит имя и фамилию продавца, день недели и суммарную выручку. Данные отсортированы по порядковому номеру дня недели и name

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 select count(customer_id) as customers_count
 from customers;
 
-with tab as (
+with report_1 as (
 select 
 concat(e.first_name,' ',e.last_name) as name,
 count (s.quantity) as operations,
@@ -12,11 +12,11 @@ left join products as p on p.product_id  = s.product_id
 group by e.first_name,e.last_name
 )
 select *
-from tab
+from report_1
 order by income desc nulls last
 limit 10;
 
-with tab as (
+with report_2 as (
 select 
 concat(e.first_name,' ',e.last_name) as name,
 FLOOR (sum (s.quantity * p.price)) as revenue,
@@ -27,7 +27,24 @@ left join products as p on p.product_id  = s.product_id
 group by e.first_name,e.last_name
 )
 select name, average_income
-from tab
+from report_2
 where average_income < (select avg(revenue) from tab)
 group by name, average_income
 order by average_income asc;
+
+with report_3 as (
+select 
+concat(e.first_name,' ',e.last_name) as name, 
+to_char(s.sale_date, 'day') as weekday,
+to_char(s.sale_date, 'id') as weekday_id,
+round(sum (s.quantity * p.price),0) as income
+from employees as e
+join sales as s on s.sales_person_id = e.employee_id
+join products as p on p.product_id  = s.product_id
+group by e.first_name, e.last_name, s.sale_date
+)
+select name, weekday, sum (income)
+from report_3
+group by name, weekday_id, weekday
+order by name, weekday_id;
+
