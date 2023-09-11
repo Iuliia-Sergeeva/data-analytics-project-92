9945f9678c2cd0478d7155961c949aee3132c569
Iuliia-Sergeeva <144040954+Iuliia-Sergeeva@users.noreply.github.com>
2023-09-11 18:19:27 +0300

    Update queries.sql
    Отчет 2 содержит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам. Таблица отсортирована по выручке по возрастанию.

--- queries.sql
+++ queries.sql
@@ -16,3 +16,18 @@ from tab
 order by income desc nulls last
 limit 10;
 
+with tab as (
+select 
+concat(e.first_name,' ',e.last_name) as name,
+FLOOR (sum (s.quantity * p.price)) as revenue,
+FLOOR (avg (s.quantity * p.price)) as average_income
+from employees as e
+left join sales as s on s.sales_person_id = e.employee_id
+left join products as p on p.product_id  = s.product_id
+group by e.first_name,e.last_name
+)
+select name, average_income
+from tab
+where average_income < (select avg(revenue) from tab)
+group by name, average_income
+order by average_income asc;
